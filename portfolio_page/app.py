ca84f46c9d034a57f4a6a45291ed72ae5b76c291
patriksn <patrik.svensson.se@gmail.com>
2025-03-25 20:21:10 +0100

    Fix: Broken pdf link p2 + more
    Minor text changes, moved links to the side of picture 3

--- portfolio_page/app.py
+++ portfolio_page/app.py
@@ -107,7 +107,7 @@ projects = {
         'technologies': ['python', 'scikit-learn', 'TensorFlow'],
         'links': {
             'thesis': 'https://www.diva-portal.org/smash/record.jsf?dswid=6950&pid=diva2%3A1635754&c=1&searchType=SIMPLE&language=en&query=%22Erik+Wendel%22+%22Patrik+Svensson%22&af=%5B%5D&aq=%5B%5B%5D%5D&aq2=%5B%5B%5D%5D&aqe=%5B%5D&noOfRows=50&sortOrder=author_sort_asc&sortOrder2=title_sort_asc&onlyFullText=false&sf=all',
-            'pdf': 'pdfs/bachelors_thesis.pdf'
+            'pdf': 'pdfs/BachelorThesisSvenssonWendel.pdf'
         }
     },
     'lung-segmentation': {
